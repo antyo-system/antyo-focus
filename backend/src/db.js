381beaef21c812943de889d905b6ff94817a81b3
Antonius Prasetyo <antoniusp99@gmail.com>
2026-01-12 06:02:53 +0700

    chore: remove old backend & prisma

--- backend/src/db.js
+++
@@ -1,7 +0,0 @@
-// src/db.js
-import 'dotenv/config';
-import { PrismaClient } from '@prisma/client';
-
-// export as named + default to be safe for both import styles
-export const prisma = new PrismaClient();
-export default prisma;
